d8e8493ebeb2de64feb31b00e4407725209b86d5
ekshiv <ekshiv1896@gmail.com>
2022-06-16 23:41:19 +0530

    CI/CD example change in return

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ app=Flask(__name__)
 @app.route("/", methods=["GET", "POST"])
 
 def index():
-    return "This is Machichne Learning Project"
+    return """This is Machichne Learning Project
+    and CI/CD pipeline has been established"""
 
 if __name__ == '__main__':
     app.run(debug=True)
