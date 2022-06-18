eb438a60b2c6f8ed7fff68f4f8252c65873d9859
ekshiv <ekshiv1896@gmail.com>
2022-06-18 23:33:53 +0530

    adding comments in exception __init__

--- housing/exception/__init__.py
+++ housing/exception/__init__.py
@@ -2,19 +2,23 @@ import os
 import sys
 
 class HousingException(Exception):
-
+    
+    #sys is used for the details of where at what file, at what line error occured in program
+    #we are creating our custom error msg and sending it to Exception class
+    
     def __init__(self, error_message:Exception, error_detail:sys):
         super().__init__(error_message)
         self.error_message=HousingException.get_detailed_error_message(error_message=error_message, error_detail=error_detail )
 
 
+    #static method is used so that we dont need any object to call this function
     @staticmethod
     def get_detailed_error_message(error_message:Exception, error_detail:sys)->str:
         
         """Error_msg: Exception object
         Error_detail: Object of sys module"""
 
-        _,_ ,exec_tb = error_detail.exc_info()
+        _,_ ,exec_tb = error_detail.exc_info()     #we are taking only 3rd value of exc_info(type, value, traceback)
         line_number = exec_tb.tb_frame.f_lineno
         file_name = exec_tb.tb_frame.f_code.co_filename
         error_message =  f"Error Occured in script: [{file_name}] at line number: [{line_number}] error message: [{error_message}] "
